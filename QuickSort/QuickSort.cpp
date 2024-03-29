57e0a56dfc89ca314361ad487c3b0ceea77336a6
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2024-03-29 16:09:46 +0700

    void input cout panjang elemen array

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
 
-//array of integers to hold values
-int arr[20];
-int cmp_count = 0;
-int mov_count = 0;
+// array of integers to hold values
+int arr[20]; 
+int cmp_count = 0; // number of datamovements
+int mov_count = 0; // number of comparasion
+int n;
+
+void input() 
+{
+	while (true)
+	{
+		cout << "Masukan panjang elemen array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+}
+
